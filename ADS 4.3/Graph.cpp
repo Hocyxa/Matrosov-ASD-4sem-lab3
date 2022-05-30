e26acb036e3897c6b147e88a85e52588ac02276c
Максим Матросов <matrosov.02@bk.ru>
2022-05-30 09:55:15 +0400

    Реализация поиска в глубину и Белмана Форда для всех вершин

--- ADS 4.3/Graph.cpp
+++ ADS 4.3/Graph.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
-#include<list>
 #include<vector>
 
 template <typename TVertex, typename TEdge = double>
 class Graph
 {
 private:
+	struct deep_information
+	{
+		int color = 0;
+		TVertex prev;
+		int t_open = 0;
+		int t_close = 0;
+	};
 	struct destination
 	{
+		int id_d;
 		TVertex dest; //вершина (куда)
 		TEdge edge; // вес
 	};
 	struct vertex
 	{
-		int id;
+		int id_v;
 		TVertex data; // данные в вершине
 		std::vector<destination> dest; //куда
+		deep_information info;
 	};
 	std::vector<vertex> table;
 	int Index(TVertex Temp_Vertex)
 	{
 		for (size_t i = 0; i < table.size(); i++)
 		{
-			if (table[i].data == Temp_Vertex) return table[i].id;
+			if (table[i].data == Temp_Vertex) return table[i].id_v;
 		}
 		return -1;
 	}
@@ -32,7 +40,7 @@ public:
 		if (Index(New_data) == -1)
 		{
 			vertex new_vertex;
-			new_vertex.id = table.size();
+			new_vertex.id_v = table.size();
 			new_vertex.data = New_data;
 			table.push_back(new_vertex);
 		}
@@ -43,8 +51,9 @@ public:
 		if (Index(Source) == -1) throw "Source not exist";
 		if (Index(Dest) == -1) throw "Destenation not exist";
 		destination new_edge;
-		new_edge.dest = Index(Dest);
+		new_edge.dest = Dest;
 		new_edge.edge = Edge;
+		new_edge.id_d = Index(Dest);
 		table[Index(Source)].dest.push_back(new_edge);
 	}
 	void Delete_Edge(TVertex Source, TVertex Dest)
@@ -54,11 +63,12 @@ public:
 		if (Index_Source == -1 || Index_Dest == -1) throw "Edge not exist";
 		for (size_t i = 0; i < table[Index_Source].dest.size(); i++)
 		{
-			if (table[Index_Source].dest[i].dest + 1  == Dest) //?????
+			if (table[Index_Source].dest[i].dest.id_d  == Index_Dest)
 			{
 				for (size_t j = i; j < table[Index_Source].dest.size() - 1; j++)
 				{
 					table[Index_Source].dest[j] = table[Index_Source].dest[j + 1];
+					table[Index_Source].dest[j].id_d--;
 				}
 				table[Index_Source].dest.pop_back();
 				return;
@@ -75,7 +85,7 @@ public:
 			{
 				for (size_t j = 0; j < table[i].dest.size(); j++)
 				{
-					if (table[i].dest[j].dest == Index_Source)
+					if (table[i].dest[j].dest == Source)
 					{
 						for (size_t k = j; k < table[i].dest.size() - 1; k++)
 						{
@@ -86,16 +96,13 @@ public:
 				}
 			}
 		}
-
-
 		for (size_t i = Index_Source; i < table.size() - 1; i++)
 		{
 			table[i] = table[i + 1];
-			table[i].id--;
+			table[i].id_v--;
 		}
 		table.pop_back();
 	}
-
 	void Print()
 	{
 		for (size_t i = 0; i < table.size(); i++)
@@ -103,9 +110,89 @@ public:
 			std::cout << table[i].data << ": {";
 			for (size_t j = 0; j < table[i].dest.size(); j++)
 			{
-				std::cout << table[i].dest[j].dest + 1 << "(" << table[i].dest[j].edge << ")";
+				std::cout << table[i].dest[j].dest  << "(" << table[i].dest[j].edge << ")";
 			}
 			std::cout << "}" << std::endl;
 		}
 	}
+
+	void Deep_Search()
+	{
+		int t = 0;
+		deep(table[0], t);
+		for (size_t j = 0; j < table.size(); j++)
+		{
+			if (table[j].info.color == 0)
+			{
+				deep(table[j], t);
+			}
+		}
+	}
+	void deep(vertex v, int& t)
+	{
+		table[v.id_v].info.color = 1;
+		table[v.id_v].info.t_open = t;
+		t++;
+		for (size_t i = 0; i < table[v.id_v].dest.size(); i++)
+		{
+			if (table[table[v.id_v].dest[i].id_d].info.color == 0)
+			{
+				table[table[v.id_v].dest[i].id_d].info.prev = table[v.id_v].data;
+				deep(table[table[v.id_v].dest[i].id_d], t);
+			}
+		}
+		table[v.id_v].info.color = 2;
+		table[v.id_v].info.t_close = t;
+		std::cout << table[v.id_v].data << ", ";
+		t++;
+	}
+
+	void belman_ford(TVertex source, TVertex dest)
+	{
+		if (Index(source) == -1 || Index(dest) == -1) throw "Vertexs not exist";
+		std::vector<double> d(table.size());
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			d[i] = 10000000000;
+		}
+		d[Index(source)] = 0;
+		std::vector<std::vector<TVertex>> ways(table.size());
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			for (size_t j = 0; j < table[i].dest.size(); j++)
+			{
+				if (d[i] + (double)table[i].dest[j].edge < d[table[i].dest[j].id_d])
+				{
+					d[table[i].dest[j].id_d] = d[i] + (double)table[i].dest[j].edge;
+					ways[table[i].dest[j].id_d] = ways[i];
+					ways[table[i].dest[j].id_d].push_back(table[i].id_v + 1);
+				}
+			}
+		}
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			ways[i].push_back(table[i].id_v + 1);
+		}
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			for (size_t j = 0; j < ways[i].size(); j++)
+			{
+				std::cout << ways[i][j];
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			if (i != Index(source))
+			{
+				if (d[i] == 10000000000)
+				{
+					std::cout << std::endl << source << "->" << i + 1 << "=" << "No way";
+				}
+				else std::cout << std::endl << source << "->" << i + 1 << "=" << d[i];
+			}
+		}
+	}
+
 };
--- ADS 4.3/Source.cpp
+++ ADS 4.3/Source.cpp
@@ -10,19 +10,18 @@ int main()
 	T.Add_Vertex(2);
 	T.Add_Vertex(3);
 	T.Add_Vertex(4);
-	T.Add_Vertex(5);
 	T.Add_Edge(1, 2, 05);
 	T.Add_Edge(2, 3, 15);
 	T.Add_Edge(3, 4, 25);
 	T.Add_Edge(1, 4, 35);
 	T.Add_Edge(3, 1, 45);
-	T.Add_Edge(3, 5, 55);
-
+	T.Add_Edge(4, 2, 55);
 	T.Print();
 
-	T.Delete_Vertex(3);
+
 	std::cout << std::endl;
+	
+	T.belman_ford(1, 3);
 
-	T.Print();
 	return 0;
 }
